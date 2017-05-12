bc47bdc842705104ce07a4be217b96b9fb963b1a
Justin Myre <justin.myre@gmail.com>
2017-05-11 19:11:19 -0700

    deleted unneed function

--- app/Movie.php
+++ app/Movie.php
@@ -10,19 +10,4 @@ class Movie extends Model
     	return $this->belongsToMany('App\Genre')->withTimestamps();
     }
 
-    public static function getGenresForDropdown() {
-
-    	$movies = Movie::with('genres')->orderBy('title', 'asc')->get();
-
-    	$genreOptions = [];
-
-        foreach($movies as $movie) {
-            foreach($movie->genres as $genre) {
-                ($genreOptions[$genre['id']] = $genre->name);
-            }
-        }
-        
-        return ksort($genreOptions);
-    }
-
 }
